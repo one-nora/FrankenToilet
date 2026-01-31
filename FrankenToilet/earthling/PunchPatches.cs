fe2e01eac675668ababa2a325cad2ebf03fb1aa0
earthlingOnFire <78956999+earthlingOnFire@users.noreply.github.com>
2026-01-31 01:12:46 +0000

    feat: swapped the colors of the blue and red arms

---
+++ FrankenToilet/earthling/PunchPatches.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using FrankenToilet.Core;
+using HarmonyLib;
+
+namespace FrankenToilet.earthling;
+
+[PatchOnEntry]
+[HarmonyPatch(typeof(Punch))]
+public static class PunchPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch("Start")]
+    public static void SwapTexture(Punch __instance)
+    {
+        switch (__instance.type)
+        {
+            case FistType.Standard:
+                Texture2D? redArm = AssetBundleHelper.LoadAsset<Texture2D>("Assets/Bundles/toiletonfire/redarm.png");
+                __instance.smr.material.mainTexture = redArm;
+                break;
+            case FistType.Heavy:
+                Texture2D? blueArm = AssetBundleHelper.LoadAsset<Texture2D>("Assets/Bundles/toiletonfire/bluearm.png");
+                __instance.smr.material.mainTexture = blueArm;
+                break;
+        }
+    }
+}
