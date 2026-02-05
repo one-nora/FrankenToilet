af17756be940b973e8cb251fa9eeae7e2395f422
one-nora <193192971+one-nora@users.noreply.github.com>
2026-02-05 02:53:38 -0300

    chore: Change namespace

--- FrankenToilet/prideunique/Other/ImproveHookPoints.cs
+++ FrankenToilet/prideunique/Other/ImproveHookPoints.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 
-namespace FrankenToilet.prideunique.Other;
+namespace FrankenToilet.prideunique;
 public static class ImproveHookPoints
 {
     public static void Init()
